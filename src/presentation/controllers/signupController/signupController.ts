e99c605e59a1202b0210619909fb0f5a4c520bad
edilsonandrade <edilson.a.andrade@gmail.com>
2023-08-16 17:42:27 -0300

    feat: ensure email is valid

--- src/presentation/controllers/signupController/signupController.ts
+++ src/presentation/controllers/signupController/signupController.ts
@@ -1,9 +1,19 @@
+import { MissingParamError } from '../../errors'
 import { type Controller } from '../../protocols/controller'
+import { type EmailValidator } from '../../protocols/emailValidator'
 import { badRequest, successRequest, type HttpRequest, type HttpResponse } from '../../protocols/http'
 
 export class SignUpController implements Controller {
+  constructor (private readonly emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    if (!httpRequest.body.name) {
+    const { name } = httpRequest.body
+    if (!this.emailValidator.isValid(httpRequest.body.email)) {
+      return badRequest(new MissingParamError('Email is not valid'))
+    }
+    if (!name) {
       return badRequest(new Error('Missing param: name'))
     }
     return successRequest({})
