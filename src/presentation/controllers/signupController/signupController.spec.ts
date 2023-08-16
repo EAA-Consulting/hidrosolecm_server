c0badbe0ed24f6181c603aff708621b56195cea9
edilsonandrade <edilson.a.andrade@gmail.com>
2023-08-16 18:22:23 -0300

    test: adding tests to ensure parameters are passing and email is valid

--- src/presentation/controllers/signupController/signupController.spec.ts
+++ src/presentation/controllers/signupController/signupController.spec.ts
@@ -100,4 +100,70 @@ describe('SignUp Controller', () => {
     expect(response.statusCode).toBe(400)
     expect(response.body).toEqual(new MissingParamError('Email is not valid'))
   })
+  test('Ensure I get error 400 from Controller if I dont pass name', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'any_email',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+
+    }
+    const response = await sut.handle(httpRequest)
+    expect(response.body).toEqual(new MissingParamError('Missing param: name'))
+  })
+  test('Ensure I get error 400 from Controller if I dont pass email', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+
+    }
+    const response = await sut.handle(httpRequest)
+    expect(response.body).toEqual(new MissingParamError('Missing param: name'))
+  })
+  test('Ensure I get error 400 from Controller if I dont pass password', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email',
+        passwordConfirmation: 'any_password'
+      }
+
+    }
+    const response = await sut.handle(httpRequest)
+    expect(response.body).toEqual(new MissingParamError('Missing param: name'))
+  })
+  test('Ensure I get error 400 from Controller if I dont pass passwordConfirmation', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email',
+        password: 'any_password'
+      }
+
+    }
+    const response = await sut.handle(httpRequest)
+    expect(response.body).toEqual(new MissingParamError('Missing param: name'))
+  })
+  test('Ensure a valid email is sent to the controller', async () => {
+    const { sut, emailValidatorStub } = makeSut()
+    const spyEmail = jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'edi@gmail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    sut.handle(httpRequest)
+    expect(spyEmail).toHaveBeenCalledWith('edi@gmail.com')
+  })
 })
