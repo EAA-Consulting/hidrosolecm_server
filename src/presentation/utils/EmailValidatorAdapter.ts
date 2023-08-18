d6e75f61a3f515e20efc72107ed96d50c78aef00
edilsonandrade <edilson.a.andrade@gmail.com>
2023-08-18 13:41:04 -0300

    feat: ensure validator is validating email

---
+++ src/presentation/utils/EmailValidatorAdapter.ts
@@ -0,0 +1,7 @@
+import { type EmailValidator } from '../protocols/emailValidator'
+import validator from 'validator'
+export class EmailValidatorAdapter implements EmailValidator {
+  isValid (email: string): boolean {
+    return validator.isEmail(email)
+  }
+}
