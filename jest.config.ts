ddf7b95002380471056252b40022246a2da54904
edilsonandrade <edilson.a.andrade@gmail.com>
2023-09-13 23:00:28 -0300

    chore: add interface to be ignored by coverage

--- jest.config.ts
+++ jest.config.ts
@@ -3,12 +3,12 @@ import type { Config } from 'jest'
 const config: Config = {
   roots: ['<rootDir>/src'],
   collectCoverage: true,
+  coveragePathIgnorePatterns: ['/interfaces'],
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/**/**/index.ts', '!<rootDir>/src/domain/models/**', '!<rootDir>/src/domain/usecases/**', '!<rootDir>/src/main/**', '!<rootDir>/src/**/*-protocols.ts', '!**/protocols/**', '!**/test/**'],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
-
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
   //   "/node_modules/"
