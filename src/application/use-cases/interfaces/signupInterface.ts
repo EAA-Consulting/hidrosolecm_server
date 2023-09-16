f52207e0670cea2e681758efeb5b01a1d3724b50
edilsonandrade <edilson.a.andrade@gmail.com>
2023-09-16 09:12:07 -0300

    refactor: add correct AccountModel returned from the domain

--- src/application/use-cases/interfaces/signupInterface.ts
+++ src/application/use-cases/interfaces/signupInterface.ts
@@ -1,3 +1,5 @@
+import { type AccountModel } from '../../../Domain/model/AccountModel'
+
 export interface SignUpApplication {
-  handle: (name: string, email: string, password: string, passwordConfirmation: string) => Promise<boolean>
+  handle: (name: string, email: string, password: string, passwordConfirmation: string) => Promise<AccountModel>
 }
