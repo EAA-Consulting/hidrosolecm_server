d5dacdf4c0e31a39a9706cb4ebe9e324259ecbf9
edilsonandrade <edilson.a.andrade@gmail.com>
2023-09-16 09:13:32 -0300

    feat: add signup application

--- src/application/use-cases/signup/signup.ts
+++ src/application/use-cases/signup/signup.ts
@@ -1,16 +1,24 @@
+import { type AccountModel } from '../../../Domain/model/AccountModel'
+import { InvalidParamError } from '../../../presentation/errors'
 import { type EmailValidator } from '../../../presentation/interfaces/emailValidator'
+import { type SignUpApplication } from '../interfaces/signupInterface'
 
-export class Signup {
+export class Signup implements SignUpApplication {
   emailValidator: EmailValidator
   constructor (emailValidator: EmailValidator) {
     this.emailValidator = emailValidator
   }
 
-  async handle (name: string, email: string, password: string, passwordConfirmation: string): Promise<boolean> {
+  async handle (name: string, email: string, password: string, passwordConfirmation: string): Promise<AccountModel> {
     if (!this.emailValidator.isValid(email)) {
-      return false
+      throw new InvalidParamError('Email is not valid')
     }
 
-    return true
+    return {
+      id: '1',
+      name: 'any_name',
+      email: 'any_email',
+      password: 'any_password'
+    }
   }
 }
