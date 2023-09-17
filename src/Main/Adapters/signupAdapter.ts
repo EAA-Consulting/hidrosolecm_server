e6885f0d7a775b035827a64433af80d1c661e45b
edilsonandrade <edilson.a.andrade@gmail.com>
2023-09-17 12:15:47 -0300

    feat: signupAdapter controller fro express

---
+++ src/Main/Adapters/signupAdapter.ts
@@ -0,0 +1,15 @@
+import { type Controller } from '../../Presentation/interfaces/controller'
+
+import type { Request, RequestHandler, Response } from 'express'
+
+export const signupAdapterController = (controller: Controller): RequestHandler => {
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
+  return async (req: Request, res: Response) => {
+    console.log(`req.body ${JSON.stringify(req.body)}`)
+    const httpRequest = {
+      body: req.body
+    }
+    const httpResponse = await controller.handle(httpRequest)
+    res.status(httpResponse.statusCode).json(httpResponse.body)
+  }
+}
