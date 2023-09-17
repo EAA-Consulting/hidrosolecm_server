e2d79d65facf25fd45adcc843907b2e881903750
edilsonandrade <edilson.a.andrade@gmail.com>
2023-09-17 07:09:46 -0300

    feat: ensure an account is created

--- src/Domain/services/addAccount/addAccount.ts
+++ src/Domain/services/addAccount/addAccount.ts
@@ -1,19 +1,15 @@
-import { type Encrypter } from '../../../application/use-cases/interfaces/encrypter'
+import { type Encrypter } from '../../../Application/use-cases/interfaces/encrypter'
 import { type AccountModel } from '../../model/AccountModel'
+import { type AddAccountRepository } from '../../repositories/account/addAccountRepository'
 import { type AddAccountData } from '../interfaces/addAccount'
 
-export class Account implements Account {
-  constructor (private readonly encrypter: Encrypter) {
-
+export class AddAccount implements AddAccountRepository {
+  constructor (private readonly encrypter: Encrypter, private readonly addAccountRepository: AddAccountRepository) {
   }
 
   async add (account: AddAccountData): Promise<AccountModel> {
     const hashedPassword = await this.encrypter.encrypt(account.password)
-    const accountData = {
-      ...account,
-      password: hashedPassword,
-      id: '1'
-    }
+    const accountData = await this.addAccountRepository.add({ ...account, password: hashedPassword })
 
     return accountData
   }
