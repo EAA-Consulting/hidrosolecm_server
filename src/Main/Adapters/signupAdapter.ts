022c10431eb682805ce2ef33980f38cce45dac1f
edilsonandrade <edilson.a.andrade@gmail.com>
2023-09-21 13:31:54 -0300

    refactor: remove console log from signupAdapter

--- src/Main/Adapters/signupAdapter.ts
+++ src/Main/Adapters/signupAdapter.ts
@@ -5,7 +5,6 @@ import type { Request, RequestHandler, Response } from 'express'
 export const signupAdapterController = (controller: Controller): RequestHandler => {
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
   return async (req: Request, res: Response) => {
-    console.log(`req.body ${JSON.stringify(req.body)}`)
     const httpRequest = {
       body: req.body
     }
