d053b1f3fd505268079bb0a51fe424d7139b106c
edilsonandrade <edilson.a.andrade@gmail.com>
2023-09-21 22:04:51 -0300

    refactor: renamed caused remove of old name file

--- src/Main/routes/signupRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router, type Express } from 'express'
-import { signupAdapterController } from '../Adapters/signupAdapter'
-import { makeSignupController } from '../factory/signup'
-export default (app: Express): void => {
-  const router = Router()
-  app.use('/api', router)
-  router.post('/signup', signupAdapterController(makeSignupController()))
-}
