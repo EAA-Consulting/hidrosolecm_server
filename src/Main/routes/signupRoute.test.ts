e038a8ffc0e76963f5e96d7853adb5adc2d7cd90
edilsonandrade <edilson.a.andrade@gmail.com>
2023-09-22 08:31:46 -0300

    refactor: signup route to delete user after test

--- src/Main/routes/signupRoute.test.ts
+++ src/Main/routes/signupRoute.test.ts
@@ -1,12 +1,32 @@
+import Firebird from 'node-firebird'
 import supertest from 'supertest'
+import { FirebirdOptions } from '../../Infrastructure/helpers/database/firebirdHelper'
 import app from '../express/app'
+
 describe('SignUp Routes', () => {
+  afterAll(async () => {
+    Firebird.attach(FirebirdOptions, (err, db) => {
+      if (err) {
+        console.log(err)
+        return
+      }
+      const sqlInsert = 'DELETE FROM USERS WHERE EMAIL = ?'
+      db.query(sqlInsert, ['signuproute@teste.com.br'], (err, result: any) => {
+        if (err) {
+          db.detach()
+          console.log(err)
+          return
+        }
+        db.detach()
+      })
+    })
+  })
   test('Should return an account on success', async () => {
     await supertest(app)
       .post('/api/signup')
       .send({
         name: 'nodetest',
-        email: 'nodetest@gmail.com',
+        email: 'signuproute@teste.com.br',
         password: '123456',
         passwordConfirmation: '123456'
       })
