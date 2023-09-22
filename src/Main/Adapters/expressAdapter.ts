2dbbc0b59365f57a6f60c0e88d5c2c8d142e87d9
edilsonandrade <edilson.a.andrade@gmail.com>
2023-09-21 22:02:09 -0300

    refactor: rename adapater for express to have a more closer name

--- src/Main/Adapters/expressAdapter.ts
+++ src/Main/Adapters/expressAdapter.ts
@@ -2,7 +2,7 @@ import { type Controller } from '../../Presentation/interfaces/controller'
 
 import type { Request, RequestHandler, Response } from 'express'
 
-export const signupAdapterController = (controller: Controller): RequestHandler => {
+export const expressAdapterController = (controller: Controller): RequestHandler => {
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
   return async (req: Request, res: Response) => {
     const httpRequest = {
