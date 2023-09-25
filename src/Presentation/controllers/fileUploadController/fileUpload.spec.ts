fb4e9dcbedb01815be780b6ca6f73bf1f1994733
edilsonandrade <edilson.a.andrade@gmail.com>
2023-09-25 15:08:33 -0300

    feat: ensure I get success message when I submit the file

--- src/Presentation/controllers/fileUploadController/fileUpload.spec.ts
+++ src/Presentation/controllers/fileUploadController/fileUpload.spec.ts
@@ -1,4 +1,5 @@
 import FormData from 'form-data'
+import path from 'path'
 import { MissingParamError } from '../../errors'
 import { type Controller } from '../../interfaces/controller'
 import { FileUploadController } from './fileUpload'
@@ -21,5 +22,17 @@ describe('File upload - Controller', () => {
 
     expect(response.statusCode).toBe(400)
     expect(response.body).toEqual(new MissingParamError('There is not file associated with the request'))
+  })
+
+  test('Ensure I successfully submit a file received 200', async () => {
+    const sut = makeSut()
+    const file = path.join(__dirname, '..', '..', 'firebirdlogo.png')
+    const formData = new FormData()
+    formData.append('file', file, 'file')
+    const httpRequest = {
+      file: formData
+    }
+    const response = await sut.handle(httpRequest)
+    expect(response.statusCode).toBe(200)
   })
 })
