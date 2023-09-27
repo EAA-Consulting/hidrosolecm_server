3b380806ec796a190dc0d0a31fdeadd3be51001d
edilsonandrade <edilson.a.andrade@gmail.com>
2023-09-27 12:36:29 -0300

    feat: add get product controller

---
+++ src/Presentation/controllers/product/getProductController.ts
@@ -0,0 +1,16 @@
+import { type IGetProduct } from '../../../Application/use-cases/interfaces/getProduct'
+import { serverError, success } from '../../helpers/httpHelpers'
+import { type Controller } from '../../interfaces/controller'
+import { type HttpRequest, type HttpResponse } from '../../interfaces/http'
+
+export class GetProductController implements Controller {
+  constructor (private readonly getProductApplication: IGetProduct) {}
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const product = await this.getProductApplication.handle()
+      return success(product)
+    } catch (error) {
+      return serverError()
+    }
+  }
+}
