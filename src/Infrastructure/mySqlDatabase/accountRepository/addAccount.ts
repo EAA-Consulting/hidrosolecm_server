0d7c15e6eae793cb47ebe4789b9c7724d38055d3
edilsonandrade <edilson.a.andrade@gmail.com>
2023-09-29 15:47:47 -0300

    feat: change add account repo to se mysql

---
+++ src/Infrastructure/mySqlDatabase/accountRepository/addAccount.ts
@@ -0,0 +1,42 @@
+import { type AccountModel } from '../../../Domain/model/AccountModel'
+import { type AddAccountRepository } from '../../../Domain/repositories/account/addAccountRepository'
+import { type AddAccountData } from '../../../Domain/services/interfaces/addAccountService'
+import { MySqlHelper } from '../../helpers/database/mysqlHelper'
+export class AddAccount implements AddAccountRepository {
+  async add (account: AddAccountData): Promise<AccountModel> {
+    return await new Promise((resolve, reject) => {
+      const mysqlPool = MySqlHelper.pool
+      const { name, email, password } = account
+      const sqlInsert = 'INSERT INTO users (NAME, EMAIL, PASSWORD) VALUES (?, ?, ?);'
+      mysqlPool.getConnection((err, mysqlConnection) => {
+        if (err) {
+          reject(new Error('Error on creating connection')); return
+        }
+        mysqlConnection.query(sqlInsert, [name, email, password], (error, result: any) => {
+          if (error) {
+            mysqlConnection.release()
+            reject(new Error('Error on insert new user')); return
+          }
+          mysqlConnection.query('SELECT name, email, password FROM users WHERE ID = ?', [result.insertId], (error, selectResult: any, fields: any) => {
+            if (error) {
+              mysqlConnection.release()
+              reject(new Error('Error on insert new user')); return
+            }
+
+            mysqlConnection.release()
+
+            if (selectResult) {
+              resolve({
+                id: result.insertId,
+                name: selectResult[0].name,
+                email: selectResult[0].email,
+                password: selectResult[0].password
+
+              })
+            }
+          })
+        })
+      })
+    })
+  }
+}
