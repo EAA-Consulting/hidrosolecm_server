8e93d8828c3081bc0ca4075950eba1ea25a88d10
edilsonandrade <edilson.a.andrade@gmail.com>
2023-09-29 15:54:21 -0300

    feat: open mysql connection when open app

--- src/Main/server.ts
+++ src/Main/server.ts
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv'
+import { MySqlHelper } from '../Infrastructure/helpers/database/mysqlHelper'
 import app from '../Main/express/app'
 dotenv.config()
 
 const port = process.env.SERVER_PORT ?? 3000
+MySqlHelper.openConnection()
 app.listen(port, () => { console.log(`Server running at http://localhost:${port}`) })
