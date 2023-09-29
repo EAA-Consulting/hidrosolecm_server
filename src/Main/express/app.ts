d1f5c298a2841cb706047ef09865989ace1f5b39
edilsonandrade <edilson.a.andrade@gmail.com>
2023-09-29 15:56:57 -0300

    feat: add server to allow cors

--- src/Main/express/app.ts
+++ src/Main/express/app.ts
@@ -1,10 +1,11 @@
 import express from 'express'
+import { cors } from '../middlewares/cors'
 import setAccountRoutes from '../routes/accountRoute'
 import setStaticRoute from '../routes/imageRoute'
 import setProduct from '../routes/productRoute'
 import setUploadRoute from '../routes/uploadRoute'
 const app = express()
-
+app.use(cors)
 app.use(express.json())
 setAccountRoutes(app)
 setUploadRoute(app)
