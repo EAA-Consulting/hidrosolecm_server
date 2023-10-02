997acc3995c5a9050ccef5e10760a123c3577d69
edilsonandrade <edilson.a.andrade@gmail.com>
2023-10-02 14:49:30 -0300

    test: ensure I can delete a product

---
+++ src/Presentation/controllers/product/deleteProductController.spec.ts
@@ -0,0 +1,42 @@
+import { type IDeleteProductApp } from '../../../Application/use-cases/interfaces/deleteProductInterface'
+import { type Controller } from '../../interfaces/controller'
+import { DeleteProductController } from './deleteProductController'
+
+describe('Delete ProductController', () => {
+  const makeApplicationStub = (): IDeleteProductApp => {
+    class DeleteProductAppStub implements IDeleteProductApp {
+      async handle (productId: number): Promise<void> {
+        await new Promise(resolve => { resolve({}) })
+      }
+    }
+    return new DeleteProductAppStub()
+  }
+  const makeSut = (): Controller => {
+    const appStub = makeApplicationStub()
+    return new DeleteProductController(appStub)
+  }
+  test('Ensure I got error 400 if I dont pass a product number', async () => {
+    const sut = makeSut()
+    const httpRequest = {
+      body: {
+        productId: ''
+      }
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: productId'))
+  })
+
+  test('Ensure I delete a product', async () => {
+    const sut = makeSut()
+    const httpRequest = {
+      body: {
+        productId: 1
+      }
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(200)
+  })
+})
