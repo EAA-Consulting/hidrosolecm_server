63431bc2aa06009a4a7ac23f75b57f15fff23284
edilsonandrade <edilson.a.andrade@gmail.com>
2023-10-02 15:43:22 -0300

    feat: creation of product delete and update router

--- src/Main/factory/product.ts
+++ src/Main/factory/product.ts
@@ -1,8 +1,13 @@
+import { DeleteProductApplication } from '../../Application/use-cases/product/deleteProductApp'
 import { GetProductApplication } from '../../Application/use-cases/product/getProductApp'
 import { ProductApplication } from '../../Application/use-cases/product/productApp'
+import { UpdateProductApp } from '../../Application/use-cases/product/updateProductApp'
+import { DeleteProductRepository } from '../../Infrastructure/mySqlDatabase/product/deleteProductRepository'
 import { GetProductRepository } from '../../Infrastructure/mySqlDatabase/product/getProductRepository'
 import { ProductRepository } from '../../Infrastructure/mySqlDatabase/product/productRepository'
+import { UpdateProductRepository } from '../../Infrastructure/mySqlDatabase/product/updateProductRepository'
 import { AddProductController } from '../../Presentation/controllers/product/addProductController'
+import { DeleteProductController } from '../../Presentation/controllers/product/deleteProductController'
 import { GetProductController } from '../../Presentation/controllers/product/getProductController'
 import { type Controller } from '../../Presentation/interfaces/controller'
 
@@ -18,4 +23,18 @@ export const makeGetProductController = (): Controller => {
   const getProductApplication = new GetProductApplication(getProductRepository)
 
   return new GetProductController(getProductApplication)
+}
+
+export const makeDeleteProductController = (): Controller => {
+  const deleteProductRepository = new DeleteProductRepository()
+  const deleteProductApplication = new DeleteProductApplication(deleteProductRepository)
+
+  return new DeleteProductController(deleteProductApplication)
+}
+
+export const makeUpdateProductController = (): Controller => {
+  const productRepository = new UpdateProductRepository()
+  const productApplication = new UpdateProductApp(productRepository)
+
+  return new AddProductController(productApplication)
 }
--- src/Main/routes/productRoute.ts
+++ src/Main/routes/productRoute.ts
@@ -1,10 +1,12 @@
 import { Router, type Express } from 'express'
 import { expressAdapterController } from '../Adapters/expressAdapter'
-import { makeAddProductController, makeGetProductController } from '../factory/product'
+import { makeAddProductController, makeDeleteProductController, makeGetProductController, makeUpdateProductController } from '../factory/product'
 
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
   router.post('/product', expressAdapterController(makeAddProductController()))
   router.get('/product', expressAdapterController(makeGetProductController()))
+  router.put('/product', expressAdapterController(makeUpdateProductController()))
+  router.delete('/product', expressAdapterController(makeDeleteProductController()))
 }
