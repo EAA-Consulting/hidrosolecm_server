2e9992a3f084248c2bbd666765e83c9296bcbaf1
edilsonandrade <edilson.a.andrade@gmail.com>
2023-10-06 10:48:51 -0300

    test: ensure I can upload image to s3

---
+++ src/Infrastructure/fileLocalStorage/s3ImageUploadRepository.test.ts
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv'
+import fs from 'fs'
+import path from 'path'
+import { S3ImageUploadRepository } from './s3ImageUploadRepository'
+dotenv.config()
+describe('S3 Image Upload Repository', () => {
+  test('should upload image to s3', async () => {
+    const s3ImageRepository = new S3ImageUploadRepository()
+    const pathFile = path.join(__dirname, '..', '..', '..', 'firebirdlogo.png')
+    const file = await fs.promises.readFile(pathFile)
+    const fileName = path.basename(pathFile)
+
+    const response = await s3ImageRepository.handle(fileName, file)
+    expect(response.fileName).toBe(fileName)
+  })
+})
