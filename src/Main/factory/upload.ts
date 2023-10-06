131b154d9bf63dca7d0ff0f60e555dc3c93f4f8d
edilsonandrade <edilson.a.andrade@gmail.com>
2023-10-06 11:45:20 -0300

    feat: change repository for upload to s3

--- src/Main/factory/upload.ts
+++ src/Main/factory/upload.ts
@@ -1,11 +1,11 @@
 import { FileUploadApp } from '../../Application/use-cases/fileUpload/fileUpload'
 import { FileUploadService } from '../../Domain/services/fileUpload/fileUploadService'
-import { ImageUploadRepository } from '../../Infrastructure/fileLocalStorage/imageUploadRepository'
+import { S3ImageUploadRepository } from '../../Infrastructure/fileLocalStorage/s3ImageUploadRepository'
 import { FileUploadController } from '../../Presentation/controllers/fileUploadController/fileUpload'
 import { type Controller } from '../../Presentation/interfaces/controller'
 
 export const makeUploadController = (): Controller => {
-  const imageUploadRepo = new ImageUploadRepository()
+  const imageUploadRepo = new S3ImageUploadRepository()
   const fileUploadService = new FileUploadService(imageUploadRepo)
   const fileUploadApp = new FileUploadApp(fileUploadService)
   return new FileUploadController(fileUploadApp)
