efa9d4eec25824cbb0b91a02bbfba6415a934c09
edilsonandrade <edilson.a.andrade@gmail.com>
2023-10-07 00:06:49 -0300

    test:  add security to the product routes

--- src/Main/routes/deleteProductRoute.test.ts
+++ src/Main/routes/deleteProductRoute.test.ts
@@ -9,10 +9,16 @@ describe('Delete route', () => {
     MySqlHelper.closeConnection()
   })
   test('Should return 200 on delete', async () => {
+    const signinResponse = await supertest(app).get('/api/signin').send({
+      email: 'teste@email.com',
+      password: '123456'
+    })
     const idToDelete: number = 0
     const deleteResponse =
+
     await supertest(app)
-      .delete(`/api/product/${idToDelete}`).send()
+    // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+      .delete(`/api/product/${idToDelete}`).set('Authorization', `Bearer ${signinResponse.body.token}`).send()
 
     expect(deleteResponse.status).toBe(200)
   })
