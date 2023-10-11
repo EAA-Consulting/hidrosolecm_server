bc1be57aeab60bcc4a3d361f4679ec51c8d86a78
edilsonandrade <edilson.a.andrade@gmail.com>
2023-10-10 22:35:05 -0300

    feat: adding https url

--- src/Main/server.ts
+++ src/Main/server.ts
@@ -1,8 +1,22 @@
 import dotenv from 'dotenv'
+import https from 'https'
 import { MySqlHelper } from '../Infrastructure/helpers/database/mysqlHelper'
 import app from '../Main/express/app'
 dotenv.config()
 
-const port = process.env.SERVER_PORT ?? 3000
+const port = process.env.PORT ?? 3333
+const env = process.env.NODE_ENV === 'development' ? 'http://localhost' : 'https://localhost'
 MySqlHelper.openConnection()
-app.listen(port, () => { console.log(`Server running at http://localhost:${port}`) })
+// make it to run also on https
+
+if (process.env.NODE_ENV === 'development ') {
+  app.listen(port, () => { console.log(`Server running at ${env}:${port}`) })
+} else {
+  const secPath = process.env.SEC_PATH ?? ''
+  const options = {
+    key: `${secPath}/private.key`,
+    cert: `${secPath}/cert.pem`
+  }
+  const httpServer = https.createServer(options, app)
+  httpServer.listen(port, () => { console.log(`Server running at ${env}:${port}`) })
+}
