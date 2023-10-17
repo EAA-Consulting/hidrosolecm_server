aec88f40bd0865e8bc5671ec4d432f1f827d5b5b
edilsonandrade <edilson.a.andrade@gmail.com>
2023-10-17 20:21:27 -0300

    chore: setup test

--- jest.config.ts
+++ jest.config.ts
@@ -5,7 +5,7 @@ const config: Config = {
   collectCoverage: true,
   coveragePathIgnorePatterns: ['/interfaces'],
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/**/**/authenticatedUser.ts', '!<rootDir>/src/**/Main/server.ts', '!<rootDir>/src/**/**/index.ts', '!<rootDir>/src/Domain/model/**', '!<rootDir>/src/Domain/repositories/**', '!<rootDir>/src/Infrastructure/**/*.ts', '!**/test/**', '!<rootDir>/src/Domain/DTOs/*.ts'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/**/**/authenticatedUser.ts', '!<rootDir>/src/**/Main/server.ts', '!<rootDir>/src/**/**/index.ts', '!<rootDir>/src/Domain/model/**', '!<rootDir>/src/Domain/repositories/**', '!<rootDir>/src/Infrastructure/**/*Helper.ts', '!**/test/**', '!<rootDir>/src/Domain/DTOs/*.ts'],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
@@ -24,7 +24,8 @@ const config: Config = {
   testMatch: [
     '**/__tests__/**/*.[jt]s?(x)',
     '**/?(*.)+(spec|test).[jt]s?(x)'
-  ]
+  ],
+  setupFilesAfterEnv: ['<rootDir>/setupTests.ts']
 
 }
 
