3e6486412499eb2bbaab6d33a639033c52f668a6
edilsonandrade <edilson.a.andrade@gmail.com>
2023-10-17 14:56:33 -0300

    feat: ensure decorate call log repository with the correct value

--- src/Main/decorators/logDecoratorController.ts
+++ src/Main/decorators/logDecoratorController.ts
@@ -1,12 +1,13 @@
+import { type ILogErrorRepository } from '../../Domain/repositories/logError/LogErrorRepositoryInterface'
 import { type Controller } from '../../Presentation/interfaces/controller'
 import { type HttpRequest, type HttpResponse } from '../../Presentation/interfaces/http'
 
 export class LogDecoratorController implements Controller {
-  constructor (private readonly controller: Controller) {}
+  constructor (private readonly controller: Controller, private readonly logRepository: ILogErrorRepository) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const httpResponse = await this.controller.handle(httpRequest)
     if (httpResponse.statusCode === 500) {
-      console.log(httpResponse.body)
+      await this.logRepository.log(httpResponse.body.stack, httpResponse.body.message)
     }
     return httpResponse
   }
