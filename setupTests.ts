11d04e90a5d26c88e8778a747857a82c63f55a7d
edilsonandrade <edilson.a.andrade@gmail.com>
2023-10-17 20:26:50 -0300

    feat: remove after all from setuptest

--- setupTests.ts
+++ setupTests.ts
@@ -3,6 +3,3 @@ import { MySqlHelper } from './src/Infrastructure/helpers/database/mysqlHelper'
 beforeAll(async () => {
   await MySqlHelper.openConnection()
 })
-afterAll(async () => {
-  await MySqlHelper.closeConnection()
-})
