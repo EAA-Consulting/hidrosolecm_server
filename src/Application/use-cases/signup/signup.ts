18265b388b9648e9040a5e997c140e2f284b2bbe
edilsonandrade <edilson.a.andrade@gmail.com>
2023-11-15 15:53:00 -0300

    refactor: remove email validation from the signup application

--- src/Application/use-cases/signup/signup.ts
+++ src/Application/use-cases/signup/signup.ts
@@ -1,17 +1,11 @@
 import { type AccountModel } from '../../../Domain/model/AccountModel'
 import { type AddAccountServices } from '../../../Domain/services/interfaces/addAccountService'
-import { InvalidParamError } from '../../../Presentation/errors'
-import { type EmailValidator } from '../../../Presentation/interfaces/emailValidator'
 import { type SignUpApplication } from '../interfaces/signupInterface'
 export class Signup implements SignUpApplication {
-  constructor (private readonly emailValidator: EmailValidator, private readonly addAccount: AddAccountServices) {
+  constructor (private readonly addAccount: AddAccountServices) {
   }
 
   async handle (name: string, email: string, password: string): Promise<AccountModel> {
-    if (!this.emailValidator.isValid(email)) {
-      throw new InvalidParamError('Email is not valid')
-    }
-
     return await this.addAccount.add({
       name,
       email,
