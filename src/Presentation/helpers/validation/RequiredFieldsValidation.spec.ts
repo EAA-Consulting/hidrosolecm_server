a811f89abe93c12e2b033a477b818fe007b1b3f1
edilsonandrade <edilson.a.andrade@gmail.com>
2023-11-16 12:23:02 -0300

    test: ensure get required field validation returns null if field is passed

--- src/Presentation/helpers/validation/RequiredFieldsValidation.spec.ts
+++ src/Presentation/helpers/validation/RequiredFieldsValidation.spec.ts
@@ -8,4 +8,10 @@ describe('Required field validation', () => {
     const error = sut.validate({ name: 'name' })
     expect(error).toEqual(new MissingParamError('field'))
   })
+  test('Return null if field is passed', () => {
+    const sut = new RequiredFieldsValidation('field')
+
+    const error = sut.validate({ field: 'field' })
+    expect(error).toBeFalsy()
+  })
 })
