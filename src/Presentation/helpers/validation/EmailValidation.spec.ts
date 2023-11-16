70b126b873dc44180d54c7f5d0f92a58fc7e9eb5
edilsonandrade <edilson.a.andrade@gmail.com>
2023-11-16 12:08:06 -0300

    test: ensure email validation returns null if email is valid

--- src/Presentation/helpers/validation/EmailValidation.spec.ts
+++ src/Presentation/helpers/validation/EmailValidation.spec.ts
@@ -8,4 +8,9 @@ describe('Email Validation', () => {
     const error = sut.validate({ email: 'invalid_email' })
     expect(error).toEqual(new InvalidParamError('Email is not valid'))
   })
+  test('Should return NULL if EmailValidator is valid', () => {
+    const sut = new EmailValidation('email', new EmailValidatorAdapter())
+    const error = sut.validate({ email: 'valid@gmail.com' })
+    expect(error).toBeFalsy()
+  })
 })
