0349c75b7f763da2f1df8c9791ae59be02588373
edilsonandrade <edilson.a.andrade@gmail.com>
2023-11-16 11:37:33 -0300

    refactor: use fieldname to validate email

--- src/Presentation/helpers/validation/EmailValidation.ts
+++ src/Presentation/helpers/validation/EmailValidation.ts
@@ -5,7 +5,7 @@ import { type Validation } from './Validation'
 export class EmailValidation implements Validation {
   constructor (private readonly fieldName: string, private readonly emailValidator: EmailValidator) {}
   validate (input: any): Error | null {
-    if (!this.emailValidator.isValid(input)) {
+    if (!this.emailValidator.isValid(input[this.fieldName])) {
       return new InvalidParamError('Email is not valid')
     }
     return null
