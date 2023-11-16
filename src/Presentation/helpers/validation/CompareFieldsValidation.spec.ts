adaa790e6c9aadd802a5837950a20f6414664e14
edilsonandrade <edilson.a.andrade@gmail.com>
2023-11-16 11:31:36 -0300

    test: ensure Compare fields validation returns InvalidParamError

---
+++ src/Presentation/helpers/validation/CompareFieldsValidation.spec.ts
@@ -0,0 +1,12 @@
+import { InvalidParamError } from '../../errors'
+import { CompareFieldsValidation } from './CompareFieldsValidation'
+
+describe('Compare fields validation', () => {
+  test('Return error Invalid param error if fiels are not equal', () => {
+    const sut = new CompareFieldsValidation('password', 'passwordConfirmation')
+
+    const error = sut.validate({ password: '1234', passwordConfirmation: '12' })
+
+    expect(error).toEqual(new InvalidParamError('passwordConfirmation'))
+  })
+})
